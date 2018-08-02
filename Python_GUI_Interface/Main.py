1bdf8be4994c30bd67fa72573e3102fde3b5aefd
MaximeJolySiemens <maxime.joly@siemens.com>
2018-08-02 09:01:13 +0200

    Adding some features and propose pop up in case of bad file selection

--- Python_GUI_Interface/Main.py
+++ Python_GUI_Interface/Main.py
@@ -6,17 +6,24 @@ Created on Wed Aug  1 18:05:54 2018
 @author: maxime
 """
 
-from tkinter import *
-from tkinter import filedialog
+from Tkinter import *
+import tkFileDialog as filedialog
 
+vtkFilename = ""
+meshFilename = ""
+x_min = 0
+x_max = 0
+y_min = 0
+y_max = 0
+Delta = 0
 
 def get_entry_fields():
    
    text_file = open("SNGM_config", "w")
    text_file.write("[meanflow]" + "\n")
-   text_file.write("cfddatafile=" + "\n")
-   text_file.write("cfdmeshfile=" + "\n")
-   text_file.write("format=" + "\n")
+   text_file.write("cfddatafile=" + vtkFilename + "\n")
+   text_file.write("cfdmeshfile=" + meshFilename +  "\n")
+   text_file.write("format=vtk" + "\n")
    text_file.write("turb_C0=2.1" + "\n")
    text_file.write("beta_star=0.09" + "\n")
    text_file.write("turb_cl=0.54" + "\n")
@@ -98,7 +105,10 @@ def get_entry_fields():
    text_file.write("vorticity=1" + "\n")
    text_file.write("velocity=1" + "\n")
    text_file.write("filter=" + Filter_type.get() + "\n")
-   text_file.write("partVol=" + str(((x_max-x_min)*(y_max-y_min)/float(Particle_Number.get()))**(.5)) + "\n")
+   if float(Particle_Number.get()) > 0:
+      text_file.write("partVol=" + str(((x_max-x_min)*(y_max-y_min)/float(Particle_Number.get()))**(.5)) + "\n")
+   else :
+      text_file.write("partVol=0" + "\n")
    text_file.write("use_intermesh=0" + "\n")
    text_file.write("x_minthreshold=" + str(x_min) + "\n")
    text_file.write("x_threshold=" + str(x_max) + "\n")
@@ -143,75 +153,105 @@ def get_entry_fields():
    
    
 def openvtkfile():
-    filename = filedialog.askopenfilename(parent=Mafenetre)
-    Path_vtk.delete(0, 'end')
-    Path_vtk.insert(0,filename)
-
-    file = open(filename, "r") 
+    global vtkFilename
+    vtkFilename = filedialog.askopenfilename(parent=Mafenetre)
     
-    data = file.readlines() 
-    global x_min
-    global x_max
-    global y_min
-    global y_max
-    k = 0
-    while data[k][0:2] != "DI":
-        k+=1
-    
-    nx = ""
-    ny = ""
-    i = 0
-    while data[k][i] != " ":
-        i += 1
-    i+=1
-    while data[k][i] != " ":
-        nx+=data[k][i]
-        i += 1
-    i+=1
-    while data[k][i] != " ":
-        ny+=data[k][i]
-        i += 1
-    
-    i = 0
-    while data[k+1][i] != " ":
-        i += 1
-    i+=1
-    Delta = ""
-    
-    while data[k+1][i] != " ":
-        Delta+=data[k+1][i]
-        i += 1
-        
-    i = 0
-    while data[k+2][i] != " ":
-        i += 1
-    i+=1
-    x_min = ""
-    y_min = ""
-    
-    while data[k+2][i] != " ":
-        x_min+=data[k+2][i]
-        i += 1
-    i+=1
-    while data[k+2][i] != " ":
-        y_min+=data[k+2][i]
-        i += 1
-    
-    Delta = float(Delta)
-    x_min = float(x_min)
-    y_min = float(y_min)
-    nx = int(nx)
-    ny = int(ny)
-    x_max = x_min+(nx-1)*Delta
-    y_max = y_min+(ny-1)*Delta
-    Label(Mafenetre, text="x_min = " + str(x_min)).grid(row=3, column=0, sticky=W)
-    Label(Mafenetre, text="y_min = " + str(y_min)).grid(row=4, column=0, sticky=W)
-    Label(Mafenetre, text="x_max = " + str(x_max)).grid(row=3, column=0, sticky=W, padx = 100)
-    Label(Mafenetre, text="y_max = " + str(y_max)).grid(row=4, column=0, sticky=W, padx = 100)
-    Label(Mafenetre, text="Mesh size = " + str(Delta)).grid(row=5, column=0, sticky=W)
+    if len(vtkFilename) > 3:
+       if vtkFilename[len(vtkFilename)-4:len(vtkFilename)] != '.vtk':
+          PopUp = Tk()
+          PopUp.geometry("300x75")
+          PopUp.title('Error Vtk open file')
+          Label(PopUp, text="Please insert valid vtk file", font='Helvetica 18 bold').pack()
+          Button(PopUp, text='Close', command=PopUp.destroy).pack()
+          PopUp.mainloop()
+       else:
+          Path_vtk.delete(0, 'end')
+          Path_vtk.insert(0,vtkFilename)
+          file = open(vtkFilename, "r") 
+          
+          data = file.readlines() 
+          global x_min
+          global x_max
+          global y_min
+          global y_max
+          k = 0
+          while data[k][0:2] != "DI":
+              k+=1
+          
+          nx = ""
+          ny = ""
+          i = 0
+          while data[k][i] != " ":
+              i += 1
+          i+=1
+          while data[k][i] != " ":
+              nx+=data[k][i]
+              i += 1
+          i+=1
+          while data[k][i] != " ":
+              ny+=data[k][i]
+              i += 1
+          
+          i = 0
+          while data[k+1][i] != " ":
+              i += 1
+          i+=1
+          Delta = ""
+          
+          while data[k+1][i] != " ":
+              Delta+=data[k+1][i]
+              i += 1
+              
+          i = 0
+          while data[k+2][i] != " ":
+              i += 1
+          i+=1
+          x_min = ""
+          y_min = ""
+          
+          while data[k+2][i] != " ":
+              x_min+=data[k+2][i]
+              i += 1
+          i+=1
+          while data[k+2][i] != " ":
+              y_min+=data[k+2][i]
+              i += 1
+          
+          Delta = float(Delta)
+          x_min = float(x_min)
+          y_min = float(y_min)
+          nx = int(nx)
+          ny = int(ny)
+          x_max = x_min+(nx-1)*Delta
+          y_max = y_min+(ny-1)*Delta
+          Label(Mafenetre, text="x_min = " + str(x_min)).grid(row=3, column=0, sticky=W)
+          Label(Mafenetre, text="y_min = " + str(y_min)).grid(row=4, column=0, sticky=W)
+          Label(Mafenetre, text="x_max = " + str(x_max)).grid(row=3, column=0, sticky=W, padx = 100)
+          Label(Mafenetre, text="y_max = " + str(y_max)).grid(row=4, column=0, sticky=W, padx = 100)
+          Label(Mafenetre, text="Mesh size = " + str(Delta)).grid(row=5, column=0, sticky=W)
    
     
     
+
+
+
+def openmeshfile():
+    global meshFilename
+    meshFilename = filedialog.askopenfilename(parent=Mafenetre)
+    
+    if len(meshFilename) > 4:
+       if meshFilename[len(meshFilename)-5:len(meshFilename)] != '.mesh':
+          PopUp = Tk()
+          PopUp.geometry("350x75")
+          PopUp.title('Error Mesh open file')
+          Label(PopUp, text="Please insert valid mesh file", font='Helvetica 18 bold').pack()
+          Button(PopUp, text='Close', command=PopUp.destroy).pack()
+          PopUp.mainloop()
+       else:
+          Path_mesh.delete(0, 'end')
+          Path_mesh.insert(0,meshFilename)
+
+
     
    
    
@@ -226,10 +266,16 @@ Label(Mafenetre, text="Meanflow parameters", font='Helvetica 18 bold').grid(row=
 Label(Mafenetre, text="Open vtk file : ").grid(row=1, column=0, sticky=W)
 ButtonVtk = Button(Mafenetre, text="Directory", command=openvtkfile).grid(row=1,column=0, padx=100, sticky=W)
 Path_vtk = Entry(Mafenetre)
-Path_vtk.config(width=15)
+Path_vtk.config(width=20)
 Path_vtk.grid(row=2,column=0, sticky=W)
 Path_vtk.insert(0,"Empty")
 
+Label(Mafenetre, text="Open mesh file : ").grid(row=6, column=0, sticky=W)
+ButtonMesh = Button(Mafenetre, text="Directory", command=openmeshfile).grid(row=6,column=0, padx=100, sticky=W)
+Path_mesh = Entry(Mafenetre)
+Path_mesh.config(width=20)
+Path_mesh.grid(row=7,column=0, sticky=W)
+Path_mesh.insert(0,"Empty")
 
 
 
@@ -242,12 +288,12 @@ Seed = IntVar()
 Checkbutton(Mafenetre, text="Constant seed", variable=Seed).grid(row=1, column = 1, sticky=W)
 Label(Mafenetre, text="Particle number : ").grid(row=2, column=1, sticky=W)
 Particle_Number = Entry(Mafenetre)
-Particle_Number.config(width=5)
+Particle_Number.config(width=7)
 Particle_Number.grid(row=2, column=1, sticky=W, padx=144)
 Particle_Number.insert(0,"0")
-Label(Mafenetre, text="Lenght Scale limiter : ").grid(row=3, column=1, sticky=W)
+Label(Mafenetre, text="Lenght scale limiter : ").grid(row=3, column=1, sticky=W)
 Lambda_limiter = Entry(Mafenetre)
-Lambda_limiter.config(width=5)
+Lambda_limiter.config(width=7)
 Lambda_limiter.grid(row=3, column=1, sticky=W, padx=144)
 Lambda_limiter.insert(0,"0")
 
@@ -282,40 +328,6 @@ Final_time.insert(0,"0")
 
 
 
-Label(Mafenetre, text="Evaluation parameters", font='Helvetica 18 bold').grid(row=6, column=0, sticky=W)
-
-Label(Mafenetre, text="Filter type : ").grid(row=7, column=0, sticky=W)
-Filter_type = StringVar()
-Radiobutton(Mafenetre, text="Gaussian", variable=Filter_type, value="Gaussian").grid(row=8, column = 0, sticky=W)
-Radiobutton(Mafenetre, text="Liepmann", variable=Filter_type, value="Liepmann").grid(row=9, column = 0, sticky=W)
-Radiobutton(Mafenetre, text="VonKarman", variable=Filter_type, value="VonKarman").grid(row=10, column = 0, sticky=W)
-
-Label(Mafenetre, text="Scaling type : ").grid(row=11, column=0, sticky=W)
-Scaling_type = StringVar()
-Radiobutton(Mafenetre, text="Particle", variable=Scaling_type, value="particle").grid(row=12, column = 0, sticky=W)
-Radiobutton(Mafenetre, text="Grid-Particle", variable=Scaling_type, value="kGrid_lambdaParticle").grid(row=13, column = 0, sticky=W)
-Radiobutton(Mafenetre, text="Grid", variable=Scaling_type, value="grid").grid(row=14, column = 0, sticky=W)
-Label(Mafenetre, text="Radius of consideration : ").grid(row=15, column=0, sticky=W)
-Radius_consideration = Entry(Mafenetre)
-Radius_consideration.config(width=2)
-Radius_consideration.grid(row=15, column=0, padx = 165)
-
-
-
-Label(Mafenetre, text="Windowing parameters", font='Helvetica 18 bold').grid(row=6, column=1, sticky=W)
-
-
-
-
-
-
-
-Label(Mafenetre, text="Statistics parameters", font='Helvetica 18 bold').grid(row=6, column=2, sticky=W)
-
-
-
-
-
 
 Label(Mafenetre, text="Writing parameters", font='Helvetica 18 bold').grid(row=0, column=3, sticky=W)
 
@@ -327,8 +339,60 @@ Label(Mafenetre, text="Writing parameters", font='Helvetica 18 bold').grid(row=0
 
 
 
+
+
+
+
+
+Line_second_part = 8
+
+
+Label(Mafenetre, text="Evaluation parameters", font='Helvetica 18 bold').grid(row=Line_second_part, column=0, sticky=W)
+
+Label(Mafenetre, text="Filter type : ").grid(row=Line_second_part+1, column=0, sticky=W)
+Filter_type = StringVar()
+Radiobutton(Mafenetre, text="Gaussian", variable=Filter_type, value="Gaussian").grid(row=Line_second_part+2, column = 0, sticky=W)
+Radiobutton(Mafenetre, text="Liepmann", variable=Filter_type, value="Liepmann").grid(row=Line_second_part+3, column = 0, sticky=W)
+Radiobutton(Mafenetre, text="VonKarman", variable=Filter_type, value="VonKarman").grid(row=Line_second_part+4, column = 0, sticky=W)
+
+Label(Mafenetre, text="Scaling type : ").grid(row=Line_second_part+5, column=0, sticky=W)
+Scaling_type = StringVar()
+Radiobutton(Mafenetre, text="Particle", variable=Scaling_type, value="particle").grid(row=Line_second_part+6, column = 0, sticky=W)
+Radiobutton(Mafenetre, text="Grid-Particle", variable=Scaling_type, value="kGrid_lambdaParticle").grid(row=Line_second_part+7, column = 0, sticky=W)
+Radiobutton(Mafenetre, text="Grid", variable=Scaling_type, value="grid").grid(row=Line_second_part+8, column = 0, sticky=W)
+Label(Mafenetre, text="Radius of consideration : ").grid(row=Line_second_part+9, column=0, sticky=W)
+Radius_consideration = Entry(Mafenetre)
+Radius_consideration.config(width=2)
+Radius_consideration.grid(row=Line_second_part+9, column=0, padx = 165)
+Radius_consideration.insert(0,"0")
+
+
+
+
+Label(Mafenetre, text="Windowing parameters", font='Helvetica 18 bold').grid(row=Line_second_part, column=1, sticky=W)
+
+
+
+
+
+
+
+Label(Mafenetre, text="Statistics parameters", font='Helvetica 18 bold').grid(row=Line_second_part, column=2, sticky=W)
+
+
+
+
+
+
+
+
+
+
+
+
+
 #Bouton quitter
-Button(Mafenetre, text='Quit', command=Mafenetre.destroy).grid(row=25, column=0, sticky=W)
+Button(Mafenetre, text='Close', command=Mafenetre.destroy).grid(row=25, column=0, sticky=W)
 Button(Mafenetre, text='Make File', command=get_entry_fields).grid(row=25, column=0, sticky=W, padx=50)
 
 
